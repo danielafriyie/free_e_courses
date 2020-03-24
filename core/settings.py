2667b0697d7934277fdedf7cc20da8c4b905efe5
Daniel Afriyie <afriyiedaniel1@outlook.com>
2020-03-24 02:50:26 +0000

    made some changes

--- core/settings.py
+++ core/settings.py
@@ -73,14 +73,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -91,7 +92,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 
 
 # Password validation
@@ -147,6 +148,6 @@ MESSAGE_TAGS = {
 
 # GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'courses/credentials.json'
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-STATICFILES_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+# STATICFILES_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = '3E061im87aAAAAAAAAAAPn1IPQanGu925rRbK-s9Cc-BXdmWzZlqjevSdY5cVGC_'
 # DROPBOX_ROOT_PATH = 'Free-Courses'
